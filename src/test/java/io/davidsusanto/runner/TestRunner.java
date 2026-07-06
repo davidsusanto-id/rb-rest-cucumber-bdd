65976737261658efc5f1940753d2dc5cd16f24cb
David Susanto <98997881+davidsusanto-id@users.noreply.github.com>
2026-07-06 21:00:50 +0700

    add test runner (#2)
    * update minimum gradle script
    
    * add test runner

---
+++ src/test/java/io/davidsusanto/runner/TestRunner.java
@@ -0,0 +1,25 @@
+package io.davidsusanto.runner;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+/**
+ * JUnit 5 Platform Suite entry point that discovers and executes all Cucumber feature files
+ * under src/test/resources/features.
+ */
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "io.davidsusanto.steps")
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:build/cucumber-report.html, json:build/cucumber-report.json, junit:build/cucumber-junit.xml")
+public class TestRunner {
+}
