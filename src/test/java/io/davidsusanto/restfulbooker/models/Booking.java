e5b7d1add19077fbb6c68acfca59d708146d069f
David Susanto <davidsusanto.id@gmail.com>
2026-07-08 20:35:15 +0700

    develop

---
+++ src/test/java/io/davidsusanto/restfulbooker/models/Booking.java
@@ -0,0 +1,75 @@
+package io.davidsusanto.restfulbooker.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Booking {
+
+    private String firstname;
+    private String lastname;
+    private Integer totalprice;
+    private Boolean depositpaid;
+    private BookingDates bookingdates;
+    private String additionalneeds;
+
+    public Booking() {
+
+    }
+
+    public Booking(String firstname, String lastname, Integer totalprice, Boolean depositpaid, BookingDates bookingdates, String additionalneeds) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.totalprice = totalprice;
+        this.depositpaid = depositpaid;
+        this.bookingdates = bookingdates;
+        this.additionalneeds = additionalneeds;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(Integer totalprice) {
+        this.totalprice = totalprice;
+    }
+
+    public Boolean getDepositpaid() {
+        return depositpaid;
+    }
+
+    public void setDepositpaid(Boolean depositpaid) {
+        this.depositpaid = depositpaid;
+    }
+
+    public BookingDates getBookingdates() {
+        return bookingdates;
+    }
+
+    public void setBookingdates(BookingDates bookingdates) {
+        this.bookingdates = bookingdates;
+    }
+
+    public String getAdditionalneeds() {
+        return additionalneeds;
+    }
+
+    public void setAdditionalneeds(String additionalneeds) {
+        this.additionalneeds = additionalneeds;
+    }
+}
--- src/test/java/io/davidsusanto/restfulbooker/specs/RequestSpecFactory.java
+++ src/test/java/io/davidsusanto/restfulbooker/specs/RequestSpecFactory.java
@@ -4,20 +4,22 @@ import io.davidsusanto.restfulbooker.config.ConfigManager;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 /**
  * Centralized REST Assured request specification.
  * Keeps base URI, headers, content type, and logging consistent across all API clients.
  */
-public class RequestSpecFactory {
+public final class RequestSpecFactory {
+
+    private RequestSpecFactory() {
+    }
 
     public static RequestSpecification request() {
         return new RequestSpecBuilder()
                 .setBaseUri(ConfigManager.baseUri())
-                .setContentType(ContentType.JSON)
-                .setAccept(ContentType.JSON)
+                .setContentType("application/json")
+                .setAccept("application/json")
                 .addFilter(new RequestLoggingFilter())
                 .addFilter(new ResponseLoggingFilter())
                 .build();
